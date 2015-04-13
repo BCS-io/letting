ad13da11c99f331d0f704bf1be5cf68abdd284d2
Richard Wigley <richard.wigley@gmail.com>
2015-04-13 19:15:02 +0100

    Database cleaner into a support file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,26 +12,5 @@ RSpec.configure do |config|
   config.order = 'random'
   Zonebie.set_random_timezone
 
-  # Database Cleaner
-  config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.strategy = :transaction
-  end
-
-  config.before(:each, js: true) do
-    DatabaseCleaner.strategy = :truncation
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
-
   config.filter_run_excluding broken: true
 end
