92e0a81f062bacfd3eb1a1af21b4e034700a8486
Richard <richard.wigley@gmail.com>
2015-06-22 21:48:32 +0100

    cron changed back to running once a day Additional comments

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,14 @@
-# file to define cron jobs
+# Define Cron jobs
+#
+# Testing if cron job running as expected
+#  - check in  letting/shared/log/cron.log it is running
+#
+# In development system remove deleted_at
+# update credits set deleted_at = null;
+# update debits set deleted_at = null;
+# cap <envioronment> db:push
+# Wait till after cron has run and page down to see if deleted_at has any rows
+# filled.
 #
 # rubocop: disable Metrics/LineLength
 #
@@ -19,7 +29,7 @@ when 'development'
 else
 
   job_type :rake, %{ cd :path && :environment_variable=:environment bundle exec rake :task --silent :output }
-  every 1.minutes, at: '4:30 am' do
+  every 1.day, at: '4:30 am' do
     rake 'hide_monotonous_account_details'
   end
 end
