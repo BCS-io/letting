5e45560f07f57e63ed0e23f5ab62cf6dd0e43c9a
Richard <richard.wigley@gmail.com>
2019-01-10 18:45:29 +0000

    rails5.0 config application.rb updated

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
 # Pick the frameworks you want:
 require 'active_record/railtie'
@@ -18,19 +18,9 @@ module Letting
     # specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    # Set Time.zone default to the specified zone and make Active Record
-    # auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names.
-    # Default is UTC.
     config.time_zone = 'London'
 
     config.i18n.enforce_available_locales = true
-
-    # The default locale is :en and all translations from
-    # config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path +=
-    #   Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = 'en-GB'
 
     config.autoload_paths += Dir[Rails.root.join('app', 'models', '{**/}'),
                                  "#{config.root}/lib/**/"]
