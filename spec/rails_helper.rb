b70d7eeaca87613adeb9c448377f8f8a3a38ec3a
Richard <richard.wigley@gmail.com>
2019-01-12 14:55:43 +0000

    spec moved capybara configuration into support file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,26 +6,6 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
-require 'capybara/rspec'
-require 'capybara-screenshot/rspec'
-Capybara::Screenshot.prune_strategy = :keep_last_run
-
-Capybara.register_driver :selenium_chrome do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
-end
-
-Capybara.register_driver :headless_chrome do |app|
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: { args: %w[headless disable-gpu] }
-  )
-
-  Capybara::Selenium::Driver.new app,
-                                 browser: :chrome,
-                                 desired_capabilities: capabilities
-end
-
-Capybara.javascript_driver = :headless_chrome
-
 require 'elasticsearch/extensions/test/cluster'
 require 'yaml'
 
