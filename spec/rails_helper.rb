68321cd6d9481cc33e65039c32b1142b4ff77c39
Richard <richard.wigley@gmail.com>
2019-01-12 14:45:22 +0000

    spec abort tests if running production environment

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
+# Prevent database truncation if the environment is production
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 require 'capybara/rspec'
 require 'capybara-screenshot/rspec'
