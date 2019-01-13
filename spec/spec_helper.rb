fc206185c600983331b7b0be89f4a94d4c6302ee
Richard <richard.wigley@gmail.com>
2019-01-13 00:42:46 +0000

    rspec flag add include_chain_clauses_in_customer_matcher...
      - will be the default in rspec 4 - didn't break anything
        so not researched it

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,10 @@ require 'database_cleaner'
 require 'zonebie'
 
 RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
