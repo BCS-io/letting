4d5e83d73942ec83fae95ceee7a7d1e34fc4dd49
Richard <richard.wigley@gmail.com>
2019-01-13 00:30:44 +0000

    rspec improve migration pending protection

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -15,7 +15,12 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
-ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  puts e.to_s.strip
+  exit 1
+end
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
