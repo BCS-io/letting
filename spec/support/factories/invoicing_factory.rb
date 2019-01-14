f36dd89631411b5f937f1c208c2e3cc1e786d95a
Richard <richard.wigley@gmail.com>
2019-01-14 16:56:10 +0000

    spec invoicing_factory refactored

--- spec/support/factories/invoicing_factory.rb
+++ spec/support/factories/invoicing_factory.rb
@@ -13,10 +13,10 @@ def invoicing_create id: nil,
                      property_range: '1-100',
                      period: '2014/06/30'..'2014/08/30',
                      runs: [run_new(invoicing: nil)]
-  invoicing = Invoicing.new id: id,
-                            property_range: property_range
-  invoicing.period = period
-  invoicing.runs = runs if runs
+  invoicing = invoicing_new id: id,
+                             property_range: property_range,
+                             period: period,
+                             runs: runs
   invoicing.save!
   invoicing
 end
