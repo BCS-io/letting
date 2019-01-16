d86e34c07213196cd4834b4f3209ba42031c1138
Richard <richard.wigley@gmail.com>
2019-01-16 11:19:18 +0000

    spec system tests remove previous capybara configuration

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -2,23 +2,6 @@ require 'capybara/rspec'
 require 'capybara-screenshot/rspec'
 Capybara::Screenshot.prune_strategy = :keep_last_run
 
-# Capybara.register_driver :selenium_chrome do |app|
-#   Capybara::Selenium::Driver.new(app, browser: :chrome)
-# end
-
-# Capybara.register_driver :headless_chrome do |app|
-#   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-#     chromeOptions: { args: %w[headless disable-gpu] }
-#   )
-
-#   Capybara::Selenium::Driver.new app,
-#                                  browser: :chrome,
-#                                  desired_capabilities: capabilities
-# end
-
-Capybara.javascript_driver = :headless_chrome
-
-
 RSpec.configure do |config|
   config.before(:each, type: :system) do
     driven_by :rack_test
