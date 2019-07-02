2187795ebaff4192559bdcf05584a11994653efb
Richard <richard.wigley@gmail.com>
2019-07-03 00:14:45 +0100

    Moving servers from bcs.io to fl-adams.uk

--- terraform/project/variables.tf
+++ terraform/project/variables.tf
@@ -2,8 +2,8 @@ variable "server_name_by_workspace" {
   type = "map"
 
   default = {
-    development = "cava.bcs.io"
-    staging = "danna.bcs.io"
-    production = "eigg.bcs.io"
+    development = "cava.fl-adams.uk"
+    staging = "danna.fl-adams.uk"
+    production = "eigg.fl-adams.uk"
   }
 }
