2022bd85bb512d66955c1367fb5374275a1ae85f
Richard <richard.wigley@gmail.com>
2019-08-16 22:02:42 +0100

    rails rspec property_spec flattening nesting

--- rails/spec/models/characters/property_spec.rb
+++ rails/spec/models/characters/property_spec.rb
@@ -26,40 +26,38 @@ RSpec.describe Property, type: :model do
   end
 
   describe 'Methods' do
-    describe '#invoice' do
-      describe '[:billing_address]' do
-        context 'without agent' do
-          property = nil
-          before do
-            agent = agent_new authorized: false,
-                              entities: [Entity.new(name: 'Bel')],
-                              address: address_new(road: 'Old', town: 'York')
-            property = property_new occupiers: [Entity.new(name: 'Grace')],
-                                    address: address_new(road: 'N', town: 'Bm'),
-                                    agent: agent
-          end
-
-          it 'displays property text' do
-            expect(property.invoice[:billing_address])
-              .to eq "Grace\nN\nBm\nWest Midlands"
-          end
+    describe '#invoice - [:billing_address]' do
+      context 'without agent' do
+        property = nil
+        before do
+          agent = agent_new authorized: false,
+                            entities: [Entity.new(name: 'Bel')],
+                            address: address_new(road: 'Old', town: 'York')
+          property = property_new occupiers: [Entity.new(name: 'Grace')],
+                                  address: address_new(road: 'N', town: 'Bm'),
+                                  agent: agent
         end
 
-        context 'with agent' do
-          property = nil
-          before do
-            agent = agent_new authorized: true,
-                              entities: [Entity.new(name: 'Bel')],
-                              address: address_new(road: 'Old', town: 'York')
-            property = property_new occupiers: [Entity.new(name: 'Grace')],
-                                    address: address_new(road: 'N', town: 'Bm'),
-                                    agent: agent
-          end
+        it 'displays property text' do
+          expect(property.invoice[:billing_address])
+            .to eq "Grace\nN\nBm\nWest Midlands"
+        end
+      end
 
-          it 'displays property text' do
-            expect(property.invoice[:billing_address])
-              .to eq "Bel\nOld\nYork\nWest Midlands"
-          end
+      context 'with agent' do
+        property = nil
+        before do
+          agent = agent_new authorized: true,
+                            entities: [Entity.new(name: 'Bel')],
+                            address: address_new(road: 'Old', town: 'York')
+          property = property_new occupiers: [Entity.new(name: 'Grace')],
+                                  address: address_new(road: 'N', town: 'Bm'),
+                                  agent: agent
+        end
+
+        it 'displays property text' do
+          expect(property.invoice[:billing_address])
+            .to eq "Bel\nOld\nYork\nWest Midlands"
         end
       end
     end
