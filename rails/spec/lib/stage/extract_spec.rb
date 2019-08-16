e3b2e681a2a5ebd40fca10397d98e7a05a442227
Richard <richard.wigley@gmail.com>
2019-08-16 12:52:25 +0100

    remove import code
      - end of an era
      - previous version needed to import data from legacy system by
        csv. No longer required

--- rails/spec/lib/stage/extract_spec.rb
+++
@@ -1,8 +0,0 @@
-require_relative '../../../lib/stage/extract'
-
-RSpec.describe Extract, :stage do
-  it 'warns about using the abstract class.' do
-    expect { warn 'override match method' }.to output.to_stderr
-    Extract.new(extracts: []).match 'x', 'y'
-  end
-end
--- rails/spec/lib/stage/insert_spec.rb
+++
@@ -1,8 +0,0 @@
-require_relative '../../../lib/stage/insert'
-
-RSpec.describe Insert, :stage do
-  it 'warns about using the abstract class.' do
-    expect { warn 'override match method' }.to output.to_stderr
-    Insert.new(insert: []).sort []
-  end
-end
